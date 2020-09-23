679ce75f150d997f2adb8f083c5243254c20b9ce
Bryan Smith <bryansmith1117@gmail.com>
2020-09-23 14:04:50 -0400

    first pass at fitness

--- NQueens/src/NQueens.java
+++ NQueens/src/NQueens.java
@@ -22,15 +22,16 @@ public class NQueens {
 			population = initializePopulation(queens);
 		}
 		
+		population[1][population[1].length-1] = (int)(100*evaluateFitness(population[1]));
 		
+		System.out.println(population[1][population[1].length-1]);
 		kbd.close();
 		return;
 	}//main
 
 
 	public static int[][] initializePopulation(int queens) {
-		int[][] population = new int[queens][queens];
-		population = new int[queens][queens];
+		int[][] population = new int[queens+1][queens+1];
 		Random rand = new Random();
 		for(int i = 0; i < queens; i++) {
 			for(int j = 0; j < queens; j++) {
@@ -40,11 +41,31 @@ public class NQueens {
 		return population;
 	}//initialize
 	
+	//search the individual for conflicts from left to right, don't double count right to left.
 	public static double evaluateFitness(int[] individual) {
-		int conflicts = 0;
-		for(int i = 0; i < individual.length; i++) {
-			
+		//updated as conflicts discovered
+		double conflicts = 0;
+	
+		for(int i = 0; i < individual.length-2; i++) {
+			//variance allows me to check diagonally from left to right (+/- value)
+			int variance = 1;
+			for(int j = i+1; j < individual.length-2; j++) {
+				//if there's another queen in the same row
+				if(individual[i]==individual[j]) {
+					conflicts++;
+				}
+				//if we aren't checking out of bounds, and there's another queen on the upwards diagonal
+				else if(individual[j] + variance < (individual.length-1) && individual[j] + variance == individual[i]) {
+					conflicts++;
+				}
+				//if we aren't checking out of bounds and there's another queen on the downwards diagonal
+				else if(individual[j] - variance > 0 && individual[j] - variance == individual[i]) {
+					conflicts++;
+				}
+				variance++;
+			}
 		}
+		System.out.println(1/(1+conflicts));
 		return (1 / (1 + conflicts));
 	}
 
