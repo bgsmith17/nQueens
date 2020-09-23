1a1230402926ba54a7f919a10523716961de8a37
Bryan Smith <bryansmith1117@gmail.com>
2020-09-23 11:20:52 -0400

    began fitness evaluation

--- NQueens/src/NQueens.java
+++ NQueens/src/NQueens.java
@@ -3,44 +3,51 @@ public class NQueens {
 	
 	public static void main(String[] args) {
 		int queens;
+		int[][] population;
 		Scanner kbd = new Scanner(System.in);
 		if(args.length == 1) {
 			try {
 				queens = Integer.parseInt(args[0]);
+				population = initializePopulation(queens);
 			}
 			catch(Exception e) {
 				System.out.println("Invalid Queens entry, please select an integer value");
 				queens = kbd.nextInt();
+				population = initializePopulation(queens);
 			}
 		}//if args passed
 		else {
 			System.out.println("For how many Queens should we find a solution?");
 			queens = kbd.nextInt();
+			population = initializePopulation(queens);
 		}
-		nQueens(queens);
-		kbd.close();
 		
+		
+		kbd.close();
 		return;
 	}//main
 
-	class nQueens{
-		int[][] population;
-		nQueens(){
+
+	public static int[][] initializePopulation(int queens) {
+		int[][] population = new int[queens][queens];
+		population = new int[queens][queens];
+		Random rand = new Random();
+		for(int i = 0; i < queens; i++) {
+			for(int j = 0; j < queens; j++) {
+				population[i][j] = rand.nextInt(queens);
+			}//for j
+		}//for i
+		return population;
+	}//initialize
+	
+	public static double evaluateFitness(int[] individual) {
+		int conflicts = 0;
+		for(int i = 0; i < individual.length; i++) {
 			
 		}
-		nQueens(int queens){
-			initializePopulation(queens);
-		}
-		private void initializePopulation(int queens) {
-			population = new int[queens][queens];
-			Random rand = new Random();
-			for(int i = 0; i < queens; i++) {
-				for(int j = 0; j < queens; j++) {
-					population[i][j] = rand.nextInt(queens);
-				}//for j
-			}//for i
-		}//initialize
-	}//nQueens
+		return (1 / (1 + conflicts));
+	}
+
 	
 	/*public static void nQueens(int queens) {
 		int[][] population = new int[queens][queens];
