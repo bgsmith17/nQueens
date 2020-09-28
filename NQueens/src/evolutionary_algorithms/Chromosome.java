44fa707668bf09ab4bacb432033126ee8c7fed18
Bryan Smith <bryansmith1117@gmail.com>
2020-09-28 15:32:37 -0400

    implemented equalTo and began partiallymapped xover

--- NQueens/src/evolutionary_algorithms/Chromosome.java
+++ NQueens/src/evolutionary_algorithms/Chromosome.java
@@ -1,20 +1,21 @@
 package evolutionary_algorithms;
 
-import java.util.Random;
+import java.util.*;
 
 public class Chromosome {
-	private int[] solution;
+	private ArrayList<Integer> solution;
 	private double fitness;
 	private boolean fitnessChecked = false;
 	private Random rand = new Random();
 	
 	public Chromosome(int queens){
 		
-		this.solution = new int[queens];
+		this.solution = new ArrayList<Integer>(queens);
 		
 		for(int i = 0; i < queens; i++) {
-			this.solution[i] = rand.nextInt(queens);
+			solution.add(i);
 		}
+		Collections.shuffle(solution, rand);
 	}
 	
 	public void getFitness() {
@@ -28,17 +29,17 @@ public class Chromosome {
 	}
 	
 	public void printSolution() {
-		for(int i = 0; i < this.solution.length; i++) {
-			System.out.println(solution[i] + " ");
+		for(int i = 0; i < this.solution.size(); i++) {
+			System.out.println(solution.get(i) + " ");
 		}
 	}
 	
 	public void evaluateFitness() {
 		double conflicts = 0;
-		for(int i = 0; i < this.solution.length-1; i++) {
-			if(checkRight(i)) {
+		for(int i = 0; i < this.solution.size()-1; i++) {
+			/*if(checkRight(i)) {
 				conflicts++;
-			}
+			}*/
 			if(checkDiagonalUp(i)) {
 				conflicts++;
 			}
@@ -51,14 +52,14 @@ public class Chromosome {
 	}
 	
 	//checkX methods are all helper methods for evaluateFitness
-	private boolean checkRight(int index){ 
-		for(int i = index+1; i < solution.length; i++) {
-			if(solution[i]==solution[index]) {
+	/*private boolean checkRight(int index){ 
+		for(int i = index+1; i < solution.size(); i++) {
+			if(solution.get(i)==solution.get(index)) {
 				return true;
 			}//if
 		}//for
 		return false;
-	}
+	}*/
 	private boolean checkDiagonalUp(int index){
 		
 		/*for(int i = index+1; i < solution.length; i++) {
@@ -68,8 +69,8 @@ public class Chromosome {
 			variance++;
 		}//for*/
 		//start checking immediately to the right of specified column 
-		for(int i = index + 1; i < solution.length; i++) {
-			if((solution[index] - index)==(solution[i] - i)){
+		for(int i = index + 1; i < solution.size(); i++) {
+			if((solution.get(index) - index)==(solution.get(i) - i)){
 				return true;
 			}
 		}
@@ -78,8 +79,8 @@ public class Chromosome {
 	}//checkdiagup
 	private boolean checkDiagonalDown(int index){
 		
-		for(int i = index+1; i < solution.length; i++) {
-			if((solution[i] + i) == (solution[index]+index)){
+		for(int i = index+1; i < solution.size(); i++) {
+			if((solution.get(i) + i) == (solution.get(index)+index)){
 				return true;
 			}//if
 		}//for
@@ -87,6 +88,41 @@ public class Chromosome {
 		return false;
 	}
 	
+	private Chromosome[] partiallyMappedCrossover(Chromosome p1, Chromosome p2, Random rand, int queens) {
+		Chromosome[] children = new Chromosome[2];
+		Integer[] parent1 = p1.solution.toArray(new Integer[p1.solution.size()]);
+		Integer[] parent2 = p2.solution.toArray(new Integer[p2.solution.size()]);
+		int[] child1 = new int[p1.solution.size()];
+		int[] child2 = new int[p1.solution.size()];
+		int start = rand.nextInt(queens);
+		int end = rand.nextInt(queens);
+		
+		if(start > end) {
+			for(int i = start; i < end; i++) {
+				child1[i] = parent1[i];
+				child2[i] = parent2[i];
+			}
+		}
+		else if(start == end) {
+			child1[start] = parent1[start];
+			child2[start] = parent2[start];
+		}
+		else {
+			for(int i = end; i < start; i++) {
+				child1[i] = parent1[i];
+				child2[i] = parent2[i];
+			}
+		}
+		return children;
+	}
 	
+	public boolean equalTo(Chromosome chrome) {
+		for(int i = 0; i < chrome.solution.size(); i++) {
+			if(this.solution.get(i) != chrome.solution.get(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
